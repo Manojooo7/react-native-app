a4f587d86adfabcda8f55e9a609f8ab588235770
manojmondal9834 <manojmondal9834@gmail.com>
2026-01-18 13:09:49 +0530

    feat/welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    appFont: require("../assets/fonts/Montserrat-Regular.ttf"),
+    appFontBold: require("../assets/fonts/Montserrat-Bold.ttf"),
+    appFontSemiBold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,110 @@
-import { Text, View } from "react-native";
+import Colors from "@/services/Colors";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hii I'm Manoj Mondal From</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={{
+          width: "100%",
+          height: 270,
+          marginTop: 130,
+          marginBottom: 25,
+        }}
+      />
+      <Text style={styles.heading}>Welcome to</Text>
+      <Text style={styles.heading}>Business Directory</Text>
+
+      <View
+        style={{
+          padding: 20,
+          backgroundColor: Colors.WHITE,
+          margin: 20,
+          borderRadius: 20,
+          display: "flex",
+          flexDirection: "column",
+          gap: 5,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "appFont",
+            fontSize: 20,
+            textAlign: "center",
+          }}
+        >
+          Discover thousand local business in one place
+        </Text>
+
+        <View style={[styles.button]}>
+          <Image
+            source={require("../assets/images/google.png")}
+            style={{
+              width: 20,
+              height: 20,
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: "appFont",
+              fontSize: 18,
+              textAlign: "center",
+            }}
+          >
+            Sign in with google
+          </Text>
+        </View>
+
+        <Text
+          style={{
+            fontFamily: "appFont",
+            fontSize: 18,
+            textAlign: "center",
+            marginTop: 10,
+            textDecorationLine: "underline",
+          }}
+        >
+          skip
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.PRIMARY,
+    height: "100%",
+  },
+  heading: {
+    fontFamily: "appFont",
+    fontSize: 40,
+    // fontWeight: 700,
+    color: Colors.WHITE,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 15,
+    width: "100%",
+    borderRadius: 30,
+    height: 60,
+    backgroundColor: "#f1f1f1",
+  },
+});
